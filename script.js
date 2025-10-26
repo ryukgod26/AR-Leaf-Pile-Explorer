23cdb8db31451ad44f204a0fbb1605f57f83e5e0
ryukgod26 <ekravi26@gmail.com>
2025-10-26 07:26:14 +0000

    Added Models for Dinosaur and Leaf Pile

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ AFRAME.registerComponent('tap_to_place',{
 	const sceneEl = this.el;
 	const reticle = document.querySelector('#reticle');
 	const tree = document.querySelector('#Tree');
-	}
+	
 
 	sceneEl.addEventListener('select',()=>{
-	if reticle.getAttribute('visible'){
+	if (reticle.getAttribute('visible')){
 	const pos = reticle.getAttribute('position');
 	tree.setAttribute('position',pos);
 	tree.setAttribute('visible',true);
 	}
 	});
+    }
 
 });
